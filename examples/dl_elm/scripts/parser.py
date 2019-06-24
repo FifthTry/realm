ef977ab74e1963365a8c282b49fdc110440e317b
nilinswap <nilinswap@gmail.com>
2019-06-24 15:35:21 +0530

    preserve order and write tests

--- examples/dl_elm/scripts/parser.py
+++ examples/dl_elm/scripts/parser.py
@@ -35,7 +35,7 @@ def match(st1, st2):
     return st1==st2
 
 def find_functions(jsFile, st, st_left):
-    delimiters = r"(function|var|catch|try|console|_Platform_export"
+    delimiters = r"(function|var|catch|try|console|_Platform_"
     reg_st = (
         r"\n(?P<content>function\s+(?P<name>\w+)\s*\(.*?\{.*?\})(?=\n+"
         + delimiters
@@ -52,24 +52,47 @@ def find_functions(jsFile, st, st_left):
 def find_export(jsFile, st, st_left):
     reg_st = r"((?<!function )_Platform_export[(].*this[)][)];)"
     c = re.compile(reg_st, re.DOTALL)
-    stt = c.search(st).group()
-    print("right here", c.search(st).groups())
+    search_result = c.search(st)
+    stt=''
+    if search_result:
+        stt = search_result.group()
     jsFile.platform_export_statement = stt
     print("herez", jsFile.platform_export_statement)
     return re.sub(reg_st, "", st_left, flags=re.DOTALL)
 
 
 def find_var(jsFile, st, st_left):
-    delimiters = r"(function|var|catch|try|console|_Platform_export"
+    delimiters = r"(function|var|catch|try|console|_Platform_"
     reg_st = r"\n(?P<content>var\s+(?P<name>\S+)\s*=?.*?;)(?=\n+" + delimiters + r"))"
     c = re.compile(reg_st, re.DOTALL)
 
     lis_v = c.findall(st)
-    print("z", lis_v[0])
     for var, name, _ in lis_v:
         jsFile.identifier_map[name] = var
     return re.sub(reg_st, "", st_left, flags=re.DOTALL)
 
+
+def find_identifier(jsFile, st, st_left):
+    delimiters = r"(function|var|catch|try|console|_Platform_"
+    reg_st_fun = (
+        r"\n(?P<contentFun>function\s+(?P<nameFun>\w+)\s*\(.*?\{.*?\})(?=\n+"
+        + delimiters
+        + r"))"
+    )
+
+    reg_st_var = r"\n(?P<contentVar>var\s+(?P<nameVar>\S+)\s*=?.*?;)(?=\n+" + delimiters + r"))"
+    reg_st = reg_st_fun + '|' + reg_st_var
+    c = re.compile(reg_st, re.DOTALL)
+    lis_f = c.findall(st)
+    print("lis_f", lis_f[0])
+    for func, nameFun, _, var, nameVar, _ in lis_f:
+        if func:
+            jsFile.identifier_map[nameFun] = func
+        elif var:
+            jsFile.identifier_map[nameVar] = var
+        else:
+            pass
+    return re.sub(reg_st, "", st_left, flags=re.DOTALL)
 
 def remove_comments(st):
     reg_st = r"(/[*].*?[*]/)|(//.*?\n)"
@@ -86,8 +109,9 @@ def remove_comments(st):
 def find_try_catch(jsFile, st, st_left):
     reg_st = r"\n(?P<content>try\s*\{.*?\}\s*catch\s*\(.*?\)\s*\{.*?\}\s*)(?=\n)"
     c = re.compile(reg_st, re.DOTALL)
-    jsFile.try_catch_statement = c.search(st).group("content")
-
+    search_result = c.search(st)
+    if search_result:
+        jsFile.try_catch_statement = search_result.group("content")
     print("try catch", jsFile.try_catch_statement)
     return re.sub(reg_st, "", st_left, flags=re.DOTALL)
 
@@ -95,15 +119,15 @@ def find_try_catch(jsFile, st, st_left):
 def find_consolewarn_statement(jsFile, st, st_left):
     reg_st = r"\n(?P<content>console\.warn\s*?\(.*?\)\s*?;)(?=\n)"
     c = re.compile(reg_st, re.DOTALL)
-    jsFile.console_warn_statement = c.search(st).group("content")
+    search_result = c.search(st)
+    if search_result:
+        jsFile.console_warn_statement = search_result.group("content")
     print("console logp", jsFile.console_warn_statement)
     return re.sub(reg_st, "", st_left, flags=re.DOTALL)
 
 
-def parse(file_path):
-    content_st = ""
-    with open(file_path) as file:
-        content_st = file.read()
+def parse(content_st):
+    
 
     content_st = remove_comments(content_st)
 
@@ -119,34 +143,40 @@ def parse(file_path):
 
     jsFile = JsStruct()
     st_left = content_st
-    st_left = find_functions(jsFile, content_st, st_left)
+    #st_left = find_functions(jsFile, content_st, st_left)
 
-    st_left = find_var(jsFile, content_st, st_left)
-
+    #st_left = find_var(jsFile, content_st, st_left)
+    st_left = find_identifier(jsFile, content_st, st_left)
     st_left = find_export(jsFile, content_st, st_left)
 
     st_left = find_try_catch(jsFile, content_st, st_left)
 
     # st_left = find_consolewarn_statement(jsFile, content_st, st_left)
-
+    order_lis = []
     for k in jsFile.identifier_map:
+        order_lis.append(k)
         print(jsFile.identifier_map[k])
 
-    print("st_left ", st_left.strip())
+    print("st_left ", st_left.strip(), order_lis)
+    print("mapH", jsFile.identifier_map)
 
     with open("output.txt", "w") as f:
         f.write(st_left)
     
     return jsFile
 
-
 if __name__ == "__main__":
-    main_js = parse("../l.js")
+    '''
+    file_path = "../l.js"
+    with open(file_path) as file:
+        content_st = file.read()
+    main_js = parse(content_st)
     m_js = parse("../m.js")
     diff_js = main_js.diff(m_js)
-    print(diff_js)
+    for k in diff_js.identifier_map:
+        print(diff_js.identifier_map[k])'''
+    pass
     
-
 
 # make tolerant
 # use hash in match
---
+++ examples/dl_elm/scripts/tests.py
@@ -0,0 +1,507 @@
+import parser
+
+# tests
+
+parse_test_input = [
+	# break simple
+	('''
+function _VirtualDom_applyAttrs(domNode, attrs)
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}
+
+
+
+
+
+function _VirtualDom_applyAttrsNS(domNode, nsAttrs)
+{
+	for (var key in nsAttrs)
+	{
+		var pair = nsAttrs[key];
+		var namespace = pair.f;
+		var value = pair.o;
+
+		value
+			? domNode.setAttributeNS(namespace, key, value)
+			: domNode.removeAttributeNS(namespace, key);
+	}
+}
+
+_Platform_export({'L':{'init':author$project$L$main(
+	elm$json$Json$Decode$succeed(_Utils_Tuple0))(0)},'N':{'init':author$project$N$main(
+	elm$json$Json$Decode$succeed(_Utils_Tuple0))(0)},'M':{'init':author$project$M$main(
+	elm$json$Json$Decode$succeed(_Utils_Tuple0))(0)}});}(this));
+    '''
+	 , {
+		 '_VirtualDom_applyAttrs': '''function _VirtualDom_applyAttrs(domNode, attrs)
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}'''
+		 , '_VirtualDom_applyAttrsNS': '''function _VirtualDom_applyAttrsNS(domNode, nsAttrs)
+{
+	for (var key in nsAttrs)
+	{
+		var pair = nsAttrs[key];
+		var namespace = pair.f;
+		var value = pair.o;
+
+		value
+			? domNode.setAttributeNS(namespace, key, value)
+			: domNode.removeAttributeNS(namespace, key);
+	}
+}'''
+	 }
+	 )
+	
+	# break delimiters: function var export
+	, ('''
+function _VirtualDom_applyAttrs(domNode, attrs)
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}
+
+
+
+
+
+var author$project$L$view = function (model) {
+	return A2(
+		elm$html$Html$div,
+		_List_fromArray(
+			[
+				elm$html$Html$Attributes$id('main')
+			]),
+		_List_fromArray(
+			[
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child1')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child2')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child3')
+					]),
+				_List_fromArray(
+					[
+						A4(author$project$L$viewInput, 'text', 'Name', model.name, author$project$L$Name),
+						A4(author$project$L$viewInput, 'password', 'Password', model.password, author$project$L$Password),
+						A4(author$project$L$viewInput, 'password', 'Re-enter Password', model.passwordAgain, author$project$L$PasswordAgain),
+						author$project$L$viewValidation(model)
+					]))
+			]));
+};
+
+_Platform_export({'L':{'init':author$project$L$main(
+	elm$json$Json$Decode$succeed(_Utils_Tuple0))(0)},'N':{'init':author$project$N$main(
+	elm$json$Json$Decode$succeed(_Utils_Tuple0))(0)},'M':{'init':author$project$M$main(
+	elm$json$Json$Decode$succeed(_Utils_Tuple0))(0)}});}(this));
+    '''
+	   , {
+		   '_VirtualDom_applyAttrs': '''function _VirtualDom_applyAttrs(domNode, attrs)
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}'''
+		   , 'author$project$L$view': '''var author$project$L$view = function (model) {
+	return A2(
+		elm$html$Html$div,
+		_List_fromArray(
+			[
+				elm$html$Html$Attributes$id('main')
+			]),
+		_List_fromArray(
+			[
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child1')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child2')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child3')
+					]),
+				_List_fromArray(
+					[
+						A4(author$project$L$viewInput, 'text', 'Name', model.name, author$project$L$Name),
+						A4(author$project$L$viewInput, 'password', 'Password', model.password, author$project$L$Password),
+						A4(author$project$L$viewInput, 'password', 'Re-enter Password', model.passwordAgain, author$project$L$PasswordAgain),
+						author$project$L$viewValidation(model)
+					]))
+			]));
+};'''
+	   }
+	   )
+	
+	# break delimiters: function var var
+	, ('''
+function _VirtualDom_applyAttrs(domNode, attrs)
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}
+
+
+
+
+
+var author$project$L$view = function (model) {
+	return A2(
+		elm$html$Html$div,
+		_List_fromArray(
+			[
+				elm$html$Html$Attributes$id('main')
+			]),
+		_List_fromArray(
+			[
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child1')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child2')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child3')
+					]),
+				_List_fromArray(
+					[
+						A4(author$project$L$viewInput, 'text', 'Name', model.name, author$project$L$Name),
+						A4(author$project$L$viewInput, 'password', 'Password', model.password, author$project$L$Password),
+						A4(author$project$L$viewInput, 'password', 'Re-enter Password', model.passwordAgain, author$project$L$PasswordAgain),
+						author$project$L$viewValidation(model)
+					]))
+			]));
+};
+
+
+var author$project$L$main = elm$browser$Browser$element(
+	{init: author$project$L$init, subscriptions: author$project$L$subscriptions, update: author$project$L$update, view: author$project$L$view});
+    '''
+	   , {
+		   '_VirtualDom_applyAttrs': '''function _VirtualDom_applyAttrs(domNode, attrs)
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}'''
+		   , 'author$project$L$view': '''var author$project$L$view = function (model) {
+	return A2(
+		elm$html$Html$div,
+		_List_fromArray(
+			[
+				elm$html$Html$Attributes$id('main')
+			]),
+		_List_fromArray(
+			[
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child1')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child2')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child3')
+					]),
+				_List_fromArray(
+					[
+						A4(author$project$L$viewInput, 'text', 'Name', model.name, author$project$L$Name),
+						A4(author$project$L$viewInput, 'password', 'Password', model.password, author$project$L$Password),
+						A4(author$project$L$viewInput, 'password', 'Re-enter Password', model.passwordAgain, author$project$L$PasswordAgain),
+						author$project$L$viewValidation(model)
+					]))
+			]));
+};'''
+	   }
+	   )
+
+	#regex
+	, ('''
+function _VirtualDom_applyAttrs(domNode, attrs)
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}
+
+function _()
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}
+
+
+function z(){
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}
+
+
+var author$project$L$view = function (model) {
+	return A2(
+		elm$html$Html$div,
+		_List_fromArray(
+			[
+				elm$html$Html$Attributes$id('main')
+			]),
+		_List_fromArray(
+			[
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child1')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child2')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child3')
+					]),
+				_List_fromArray(
+					[
+						A4(author$project$L$viewInput, 'text', 'Name', model.name, author$project$L$Name),
+						A4(author$project$L$viewInput, 'password', 'Password', model.password, author$project$L$Password),
+						A4(author$project$L$viewInput, 'password', 'Re-enter Password', model.passwordAgain, author$project$L$PasswordAgain),
+						author$project$L$viewValidation(model)
+					]))
+			]));
+};
+
+
+var author$project$L$main = elm$browser$Browser$element(
+	{init: author$project$L$init, subscriptions: author$project$L$subscriptions, update: author$project$L$update, view: author$project$L$view});
+    '''
+	    ,{
+		   '_': '''function _()
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}'''
+		,'z':'''function z(){
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}'''
+
+	,'_VirtualDom_applyAttrs': '''function _VirtualDom_applyAttrs(domNode, attrs)
+{
+	for (var key in attrs)
+	{
+		var value = attrs[key];
+		value
+			? domNode.setAttribute(key, value)
+			: domNode.removeAttribute(key);
+	}
+}'''
+		   , 'author$project$L$view': '''var author$project$L$view = function (model) {
+	return A2(
+		elm$html$Html$div,
+		_List_fromArray(
+			[
+				elm$html$Html$Attributes$id('main')
+			]),
+		_List_fromArray(
+			[
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child1')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child2')
+					]),
+				_List_Nil),
+				A2(
+				elm$html$Html$div,
+				_List_fromArray(
+					[
+						elm$html$Html$Attributes$id('child3')
+					]),
+				_List_fromArray(
+					[
+						A4(author$project$L$viewInput, 'text', 'Name', model.name, author$project$L$Name),
+						A4(author$project$L$viewInput, 'password', 'Password', model.password, author$project$L$Password),
+						A4(author$project$L$viewInput, 'password', 'Re-enter Password', model.passwordAgain, author$project$L$PasswordAgain),
+						author$project$L$viewValidation(model)
+					]))
+			]));
+};'''
+	   }
+	   )
+	
+
+]
+
+
+
+
+
+diff_test_input = [
+	('''
+function A(){
+		this is A
+	}
+function B(){
+		this is B
+	}
+function C(){
+		this is C
+	}
+var
+	'''
+	 ,'''
+function A(){
+		this is A
+	}
+function B(){
+		this is B
+	}
+function C(){
+		this is C
+	}
+var
+	''', {})
+]
+
+
+def test_parse_ex():
+	def test_parse(st, result_dic):
+		assert (parser.parse(st).identifier_map == result_dic)
+	
+	for rank, (st, result_map) in enumerate(parse_test_input):
+		print("rank", rank)
+		test_parse(st, result_map)
+		
+		
+def test_diff_ex():
+	def test_diff(l, m, result_dic):
+		assert (parser.parse(l).diff(
+			parser.parse(m)).identifier_map == result_dic)
+	
+	for l, m, result_map in diff_test_input:
+		test_diff(l, m, result_map)
+	
+	
+
+if __name__ == "__main__":
+	test_parse_ex()
+	test_diff_ex()
+
+
+
+
+# make tolerant
+# use hash in match
