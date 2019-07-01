da3055e5276f36c45241dbe422812de4d815dac7
nilinswap <nilinswap@gmail.com>
2019-07-01 15:03:31 +0530

    untidy script complete

--- scripts/compile_elm.py
+++ scripts/compile_elm.py
@@ -1,34 +1,68 @@
 import os
-import sys
 from os.path import expanduser
+import re
 
 
-def compile_elm(source_name, destination_name):
-	home = expanduser("~")
+
+home = expanduser("~")
+
+def compile(source_path, destination_path):
 	
-	source_dir = home + "/forgit/dwelm/realm/examples/dl_elm/src/"
-	source_path = source_dir + source_name
+	
+	
 	
 	elm_path = 'forgit/dwelm/elm_latest/node_modules/elm/bin/elm'
 	elm_path = home + '/' + elm_path
 	
-	destination_dir = home + "/forgit/dwelm/realm/examples/basic/static/realm/elatest/"
-	destination_path = destination_dir + destination_name
-	os.chdir("../examples/dl_elm/")
 	
+	os.chdir(home + "/forgit/dwelm/realm/examples/dl_elm/")
 	print(os.getcwd(), source_path, destination_path)
 	
 	os.system(
 		elm_path + " make " + source_path + " --output " + destination_path)
 
 
-# run
+def has_main( file ):
+	with open(file) as f:
+		content_st = f.read()
+	
+
+	
+	reg_st = r"\s+?main\s*?=\s*?(Browser[.])?((sandbox)|(element))\s*\{.*?\}"
+	c = re.compile(reg_st, re.DOTALL)
+	search_result = c.search(content_st)
+	return (search_result != None)
+	
+def compile_all_elm(source_dir, destination_dir):
+
+	#handle error
+	for file in os.listdir(source_dir):
+		print(file)
+		source_path = source_dir+ '/'+file
+		dest_path = destination_dir + '/' + file
+		#if file is already present handle
+		
+		
+		if os.path.isdir(source_path):
+			
+			if not os.path.isdir(dest_path):
+				
+				os.mkdir(dest_path)
+				
+			compile_all_elm(source_path, dest_path)
+			
+		filename, file_extension = os.path.splitext(file)
+		if file_extension == '.elm' and has_main(source_path):
+			dest_path = destination_dir + '/'+  filename+".js"
+			compile(source_path, dest_path)
 
 
-compile_elm("F/M.elm", "F/M.js")
 
-# compile_elm
+source_dir = home + "/forgit/dwelm/realm/examples/dl_elm/src"
 
+destination_dir = home + "/forgit/dwelm/realm/examples/basic/static/realm/elatest"
 
-# preserve_structure
-# pick only
+print(compile_all_elm(source_dir, destination_dir))
+
+# pick only one with main
+# make everything exception friendly and neat
