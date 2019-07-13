26772a6fde1909638d12d646361576154f2a8e31
nilinswap <nilinswap@gmail.com>
2019-07-13 20:37:20 +0530

    type check

--- realm-cli/realm_cli/compile_elm.py
+++ realm-cli/realm_cli/compile_elm.py
@@ -1,12 +1,13 @@
 import os
 from os.path import expanduser
 import re
-from typing import List, Tuple, Optional, Match
+from typing import Set, List, Optional, Match
+
 
 home: str = expanduser("~")
 
 elm_path_G: str = "forgit/dwelm/elm_latest/node_modules/elm/bin/elm"
-elm_path_G= home + "/" + elm_path_G
+elm_path_G = home + "/" + elm_path_G
 
 elm_format_path_G: str = "forgit/dwelm/elm_latest/node_modules/elm-format/bin/elm-format"
 elm_format_path_G = home + "/" + elm_format_path_G
@@ -15,11 +16,11 @@ go_to_dir_G: str = home + "/forgit/dwelm/graftpress/"
 
 
 def compile(
-    source_path,
-    destination_path,
-    elm_path=elm_path_G,
-    elm_format_path=elm_format_path_G,
-    elm_proj_dir=go_to_dir_G,
+    source_path: str,
+    destination_path: str,
+    elm_path: str = elm_path_G,
+    elm_format_path: str = elm_format_path_G,
+    elm_proj_dir: str = go_to_dir_G,
 ):
 
     if elm_proj_dir:
@@ -29,24 +30,24 @@ def compile(
     os.system(elm_path + " make " + source_path + " --output " + destination_path)
 
 
-def has_main(file):
+def has_main(file: str) -> bool:
     with open(file) as f:
         content_st = f.read()
 
     reg_st = r"\s+?main\s*?=\s*?(Browser[.])?((sandbox)|(element))\s*\{.*?\}"
     c = re.compile(reg_st, re.DOTALL)
-    search_result = c.search(content_st)
+    search_result: Optional[Match[str]] = c.search(content_st)
     return search_result != None
 
 
-def populate_map(bucket, source_dir, root_path):
+def populate_set(bucket: Set[str], source_dir: str, root_path: str):
 
     for file in os.listdir(os.path.join(source_dir, root_path)):
         root_path = os.path.join(root_path, file)
-        new_source_dir = os.path.join(source_dir, root_path, file)
+        new_source_dir: str = os.path.join(source_dir, root_path, file)
         if os.path.isdir(new_source_dir):
 
-            populate_map(bucket, source_dir, root_path)
+            populate_set(bucket, source_dir, root_path)
         else:
             if root_path in bucket:
                 raise Exception(
@@ -55,25 +56,25 @@ def populate_map(bucket, source_dir, root_path):
             bucket.add(root_path)
 
 
-def check_conflicts(source_dirs):
-    bucket = set([])
+def check_conflicts(source_dirs: List[str]):
+    bucket: Set[str] = set([])
     for dir in source_dirs:
-        populate_map(bucket, dir, root_path="")
+        populate_set(bucket, dir, root_path="")
 
 
 def compile_all_elm(
-    source_dir,
-    destination_dir,
-    elm_path=elm_path_G,
-    elm_format_path=elm_format_path_G,
-    go_to_dir=go_to_dir_G,
+    source_dir: str,
+    destination_dir: str,
+    elm_path: str = elm_path_G,
+    elm_format_path: str = elm_format_path_G,
+    go_to_dir: str = go_to_dir_G,
 ):
 
     # handle error
     for file in os.listdir(source_dir):
         print("inside cae ", file)
-        source_path = source_dir + "/" + file
-        dest_path = destination_dir + "/" + file
+        source_path: str = source_dir + "/" + file
+        dest_path: str = destination_dir + "/" + file
         # if file is already present handle
 
         if os.path.isdir(source_path):
@@ -95,10 +96,10 @@ def compile_all_elm(
 
 
 if __name__ == "__main__":
-    source_dir = "src/frontend"
-    static_dir = "src/static"
-    destination_dir = static_dir + "/realm/"
-    latest_dir = open(destination_dir + "latest.txt").read()
+    source_dir: str = "src/frontend"
+    static_dir: str = "src/static"
+    destination_dir: str = static_dir + "/realm/"
+    latest_dir: str = open(destination_dir + "latest.txt").read()
     destination_dir = destination_dir + latest_dir
 
     print(compile_all_elm(source_dir, destination_dir))
--- realm-cli/realm_cli/main.py
+++ realm-cli/realm_cli/main.py
@@ -6,56 +6,52 @@ import os
 import realm_cli.compile_elm as ce
 import realm_cli.rust_json as rj
 import json
+from typing import List
 
 
 def main():
     print(sys.argv)
-    if sys.argv[1] == 'startproject':
-        project_name = 'hello'
-        if len(sys.argv) > 2 and sys.argv[2] != '':
+    if sys.argv[1] == "startproject":
+        project_name: str = "hello"
+        if len(sys.argv) > 2 and sys.argv[2] != "":
             project_name = sys.argv[2]
-            cookiecutter('gh:nilinswap/realm-startapp', extra_context={ "project_name": project_name, "project_slug": project_name}, no_input=True)
+            cookiecutter(
+                "gh:nilinswap/realm-startapp",
+                extra_context={
+                    "project_name": project_name,
+                    "project_slug": project_name,
+                },
+                no_input=True,
+            )
 
         os.chdir(project_name)
-        os.system("yarn add package.json") #make exception friendly
-        
-        curr_dir = os.getcwd()
+        os.system("yarn add package.json")  # make exception friendly
+
+        curr_dir: str = os.getcwd()
         print("curr_dir", curr_dir)
-        bin_path = os.path.join(curr_dir, "node_modules", ".bin")
-        elm_path = os.path.join(bin_path, "elm")
+        bin_path: str = os.path.join(curr_dir, "node_modules", ".bin")
+        elm_path: str = os.path.join(bin_path, "elm")
         os.system(elm_path + " install")
-        
-        
-        
-    elif sys.argv[1] == 'debug':
-        curr_dir = os.getcwd()
+
+    elif sys.argv[1] == "debug":
+        curr_dir: str = os.getcwd()
         print("curr_dir, ", curr_dir)
-        bin_path = os.path.join(curr_dir, "node_modules", ".bin")
-        elm_path = os.path.join(bin_path, "elm")
-        elm_format_path = os.path.join(bin_path, "elm-format")
-        elm_dest_dir = "src/static/realm/elatest/"
-        elm_src_dirs = ["src/frontend"]
+        bin_path: str = os.path.join(curr_dir, "node_modules", ".bin")
+        elm_path: str = os.path.join(bin_path, "elm")
+        elm_format_path: str = os.path.join(bin_path, "elm-format")
+        elm_dest_dir: str = "src/static/realm/elatest/"
+        elm_src_dirs: List[str] = ["src/frontend"]
         with open("realm.json", "r") as f:
             config = json.load(f)
-            elm_src_dirs = config["elm_source_dirs"]
-        
+            elm_src_dirs: List[str] = config["elm_source_dirs"]
+
         ce.check_conflicts(elm_src_dirs)
         for src_dir in elm_src_dirs:
-            ce.compile_all_elm(src_dir, elm_dest_dir, elm_path, elm_format_path,
-                               "")
+            ce.compile_all_elm(src_dir, elm_dest_dir, elm_path, elm_format_path, "")
         os.system("RUST_BACKTRACE=1 cargo run")
 
-    elif sys.argv[1] == 'build':
+    elif sys.argv[1] == "build":
         rj.main()
-        
-        
-        
-        
-        
-        
-            
-        
+
 
 # ToDo: make main clean; in fact, read the whole thing for grace.
-
-    
