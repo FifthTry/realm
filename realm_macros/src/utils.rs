150c14a0bfeff6a5b63c62698c0f9d2c28ac75a7
Amit Upadhyay <upadhyay@gmail.com>
2019-11-22 19:29:18 +0530

    super::swapnil

--- realm_macros/src/utils.rs
+++ realm_macros/src/utils.rs
@@ -16,21 +16,6 @@ pub fn convert_id_to_html_path(id_: &str) -> String {
 
 #[cfg(test)]
 mod tests {
-    pub fn convert_id_to_html_path(id_: &str) -> String {
-        // first separate the string on.
-        let id = String::from(id_);
-        let mut html_path;
-        let v: Vec<&str> = id.split('.').collect();
-        let mut v_: Vec<String> = vec![];
-        for sli in v.iter() {
-            let k_sli = inflector::cases::kebabcase::to_kebab_case(sli).to_lowercase();
-            v_.push(String::from(k_sli))
-            //html_path.push_str("/");
-        }
-        html_path = v_.join("/");
-        html_path.push_str(".html");
-        html_path
-    }
     #[test]
     fn cithp_test() {
         let p = "Pages.A.B";
@@ -42,8 +27,8 @@ mod tests {
         let r = "Pages.A.cC";
         let tar_d = "pages/a/c-c.html".to_string();
 
-        assert_eq!(tar_s, convert_id_to_html_path(p));
-        assert_eq!(tar_r, convert_id_to_html_path(q));
-        assert_eq!(tar_d, convert_id_to_html_path(r));
+        assert_eq!(tar_s, super::convert_id_to_html_path(p));
+        assert_eq!(tar_r, super::convert_id_to_html_path(q));
+        assert_eq!(tar_d, super::convert_id_to_html_path(r));
     }
 }
