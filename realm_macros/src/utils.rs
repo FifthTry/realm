c8206d513e69704e1f9a034fbb296763941501e1
nilinswap <nilinswap@gmail.com>
2019-11-22 17:36:00 +0530

    the error passes away

--- realm_macros/src/utils.rs
+++ realm_macros/src/utils.rs
@@ -1,14 +1,18 @@
 use inflector;
 
 pub fn convert_id_to_html_path(id_: &str) -> String {
-    // first seperate the string on.
+    // first seperate the string on .
     let id = String::from(id_);
     let mut html_path = String::new();
     let v: Vec<&str> = id.split('.').collect();
+    let mut v_: Vec<String> = vec![];
     for sli in v[1..].iter() {
         let k_sli = inflector::cases::kebabcase::to_kebab_case(sli).to_lowercase();
-        html_path.push_str(&k_sli);
+        v_.push(String::from(k_sli))
+        //html_path.push_str("/");
     }
+    html_path = v_.join("/");
+    html_path.push_str(".html");
     html_path
 }
 
